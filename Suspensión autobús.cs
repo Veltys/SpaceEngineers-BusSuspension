bc283cfc5be54528c2e10a7ea42809297cbeeaef
Veltys <veltys@veltys.es>
2022-05-21 23:25:04 +0200

    ADD: Basic code to begin

---
+++ Suspensión autobús.cs
@@ -0,0 +1,51 @@
+﻿using Sandbox.ModAPI.Ingame;
+
+
+/// <file>Suspensión autobús.cs</file>
+/// <summary>Autobus vehicle suspension manager</summary>
+/// <author>Veltys</author>
+/// <date>2022-05-21</date>
+/// <version>0.0.1</version>
+/// <note>Made just for internal use</note>
+
+
+namespace ScriptingClass {
+
+    /// <summary>
+    /// Program class
+    /// Provides the container for a programmable block program
+    /// Needs to be called "Program" because ingame programmable block assume that
+    /// </summary>
+    class Program {
+        // Some stuff for avoiding some environment errors
+        IMyGridTerminalSystem? GridTerminalSystem = null;
+        IMyGridProgramRuntimeInfo? Runtime = null;
+        Action<string>? Echo = null;
+        IMyTerminalBlock? Me = null;
+
+        // Start copying to game after this text
+
+
+        /// <summary>
+        /// Class constructor
+        /// Set-up all variables and programmable block screens
+        /// </summary>
+        public Program() {
+
+        }
+
+        /// <summary>
+        /// Main public method
+        /// Maing execution
+        /// </summary>
+        /// <param name="argument">Argument given</param>
+        /// <param name="updateSource">"Who" ran the programmable block</param>
+        public void Main(string argument, UpdateType updateSource) {
+            Echo(argument);
+        }
+
+
+        // Stop copying to game before this text
+    }
+}
+
